a6e3069694ad48b0863575ea1e8cd014247ff56f
Bruno Correa <bruno.correa@sensedia.com>
2023-11-05 13:35:15 -0300

    fix program credits

--- src/Info.java
+++ src/Info.java
@@ -3,7 +3,7 @@ import java.util.List;
 public abstract class Info {
     public static final String PROJECT_NAME = "RDBMS Grupo 10 - MariaDB - POO II";
     public static final String PROJECT_VERSION = "1.0.0";
-    public static final String PROJECT_LAST_UPDATE = "30/10/2023";
+    public static final String PROJECT_LAST_UPDATE = "05/11/2023";
     public static final String PROJECT_DESCRIPTION = "Sistema de Controle de Pedidos para a disciplina de Programação Orientada a Objetos II";
-    public static final List<String> PROJECT_AUTHORS = List.of("Bruno Ricardo Corrêa - 260759");
+    public static final List<String> PROJECT_AUTHORS = List.of("Bruno Ricardo Corrêa - 260759", "Felipe Gabriel Viana Alves - 246258", "Igor Paulo dos Santos Santana - 198643", "João Gabriel Gomes Mariano - 247487", "Julia Gonzalez Leal - 260824");
 }
